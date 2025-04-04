a7d270864b79635df24111e8c57af847e7738f9e
wp.dev <wittaya.wipratoom@gmail.com>
2025-04-04 16:10:09 +0700

    Remove unused Stocks property from Comment model

--- Models/Comments.cs
+++ Models/Comments.cs
@@ -12,6 +12,5 @@ namespace api.Models
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; } = DateTime.Now;
         public int? StockId { get; set; }
-        public Stock? Stocks { get; set; }
     }
 }
