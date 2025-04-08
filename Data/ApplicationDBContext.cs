e6b5bd8e752677e02e59a6c97c3b5380dbc34ada
wp.dev <wittaya.wipratoom@gmail.com>
2025-04-08 17:28:57 +0700

    Add user registration functionality with role seeding and DTO implementation

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -19,5 +21,25 @@ namespace api.Data
 
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            List<IdentityRole> roles =
+            [
+                new IdentityRole
+                {
+                    Id="1",
+                    Name="Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id="2",
+                    Name="User",
+                    NormalizedName="USER"
+                }
+            ];
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
