f27bd14b6310d5311bb9b3af835e5eb104432766
wp.dev <wittaya.wipratoom@gmail.com>
2025-04-10 16:17:30 +0700

    Add AppUserId and AppUser properties to Comment model for user association

--- Models/Comments.cs
+++ Models/Comments.cs
@@ -10,5 +10,7 @@ namespace api.Models
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; } = DateTime.Now;
         public int? StockId { get; set; }
+        public string? AppUserId { get; set; }
+        public AppUser? AppUser { get; set; }
     }
 }
